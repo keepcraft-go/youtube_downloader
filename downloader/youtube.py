33a689d49014b50038014d28192fc7380a5d5b02
nurikim <nr@skilleat.com>
2026-02-09 15:44:19 +0900

    change 1080p

--- downloader/youtube.py
+++ downloader/youtube.py
@@ -4,7 +4,12 @@ from yt_dlp import YoutubeDL
 def download_video(url):
     ydl_opts = {
         "outtmpl": "downloads/%(title)s.%(ext)s",
-        "format": "best"
+        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
+        # 최고화질 mp4 비디오 + 최고음질 오디오 병합
+        # 없으면 최고화질 mp4
+        # 그것도 없으면 최고 품질
+        
+        "merge_output_format": "mp4",  # 병합 시 mp4로
     }
     with YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url)
